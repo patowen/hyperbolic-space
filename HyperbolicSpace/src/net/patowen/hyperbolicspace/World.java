80c80fbae1b4b3d34e026f3c35d3dda29f6347f4
Patrick Owen <patowen95@gmail.com>
2015-02-14 20:32:38 -0500

    Clean up World

--- HyperbolicSpace/src/net/patowen/hyperbolicspace/World.java
+++ HyperbolicSpace/src/net/patowen/hyperbolicspace/World.java
@@ -1,12 +1,7 @@
 package net.patowen.hyperbolicspace;
 
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-import java.util.ArrayList;
-
 import javax.media.opengl.GL3;
 
-import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.math.FloatUtil;
 
 
@@ -17,187 +12,17 @@ public class World
 	private Vector3 pos;
 	private Orientation o;
 	
-	private ArrayList<Vertex> v;
-	
-	private FloatBuffer vertexBuffer;
-	private FloatBuffer normalBuffer;
-	
-	private IntBuffer positionBufferObject;
-	
 	private SceneNode dodeca;
 	
 	public World(InputHandler inputHandler)
 	{
 		input = inputHandler;
 		
-//		xPos = -0.9; yPos = 0; zPos = 0;
 		pos = new Vector3(0, 0, 0);
 		o = new Orientation(new Vector3(0, 0, -1), new Vector3(1, 0, 0), new Vector3(0, 1, 0));
 		
-		v = new ArrayList<Vertex>();
-//		makeHorosphere();
-//		makeDodecahedron();
-//		makeCylinder();
-		
-		vertexBuffer = Buffers.newDirectFloatBuffer(3*v.size());
-		normalBuffer = Buffers.newDirectFloatBuffer(3*v.size());
-		
 		dodeca = new Dodecahedron();
 		dodeca.reposition(pos);
-	}
-	
-	public void makeHorosphere()
-	{
-		int res = 100, focus = 10;
-		double xBase = 0, yBase = 0, zBase = 0;
-		double[][] x, y, z;
-		x = new double[res][res];
-		y = new double[res][res];
-		z = new double[res][res];
-		double[] point = new double[3];
-		point[0] = 0; point[1] = 0; point[2] = 0;
-		
-		for (int i=0; i<res/2; i++)
-		{
-			point[0] = -point[0];
-			invert(point, -focus, 0, -1, focus);
-			point[1] = -point[1];
-			invert(point, 0, -focus, -1, focus);
-		}
-		
-		xBase = point[0]; yBase = point[1]; zBase = point[2];
-		
-		for (int i=0; i<res; i++)
-		{			
-			for (int j=0; j<res; j++)
-			{	
-				point[0] = xBase; point[1] = yBase; point[2] = zBase;
-				
-				for (int ii=0; ii<i; ii++)
-				{
-					point[0] = -point[0];
-					invert(point, focus, 0, -1, focus);
-				}
-				
-				for (int jj=0; jj<j; jj++)
-				{
-					point[1] = -point[1];
-					invert(point, 0, focus, -1, focus);
-				}
-				
-				x[i][j] = point[0]; y[i][j] = point[1]; z[i][j] = point[2];
-			}
-		}
-		
-		for (int i=0; i<res; i++)
-		{
-			for (int j=0; j<res; j++)
-			{
-				if (i < res-1)
-				{
-					v.add(new Vertex(x[i][j], y[i][j], z[i][j]));
-					v.add(new Vertex(x[i+1][j], y[i+1][j], z[i+1][j]));
-				}
-				
-				if (j < res-1)
-				{
-					v.add(new Vertex(x[i][j], y[i][j], z[i][j]));
-					v.add(new Vertex(x[i][j+1], y[i][j+1], z[i][j+1]));
-				}
-			}
-		}
-	}
-	
-	private void invert(double[] point, double cx, double cy, double cz, double r)
-	{
-		double x = point[0]-cx, y = point[1]-cy, z = point[2]-cz;
-		double s = x*x + y*y + z*z;
-		
-		x *= r*r/s; y *= r*r/s; z *= r*r/s;
-		point[0] = x+cx; point[1] = y+cy; point[2] = z+cz;
-	}
-	
-	public void makeCylinder()
-	{		
-		for (int i=0; i<20; i++)
-		{
-			double theta = Math.PI*2*i/20;
-			makeLine(new Vertex(0, 0.5*Math.cos(theta), 0.5*Math.sin(theta)));
-		}
-	}
-	
-	public void makeLine(Vertex ver)
-	{
-		Vector3 back = new Vector3(-0.05, 0, 0);
-		Vector3 forth = back.times(-1);
-		
-		for (int i=0; i<80; i++)
-		{
-			ver.translate(back);
-			ver = new Vertex(ver.getVt(), ver.getNt());
-		}
-		
-		v.add(ver);
-		
-		for (int i=0; i<160; i++)
-		{
-			ver.translate(forth);
-			ver = new Vertex(ver.getVt(), ver.getNt());
-			v.add(ver);
-			
-			if (i != 159) v.add(ver);
-		}
-	}
-	
-	public void makeDodecahedron()
-	{
-		//v = new Vertex[60]
-		
-		double s = 0.3; //0.3
-		//s = Math.sqrt(1.0/3.0)-0.00005;
-		double p = (1+Math.sqrt(5))/2;
-		double q = s/p; p = s*p;
-		
-		v.add(new Vertex(s, s, s)); v.add(new Vertex(0, q, p));
-		v.add(new Vertex(0, q, p)); v.add(new Vertex(-s, s, s));
-		v.add(new Vertex(-s, s, s)); v.add(new Vertex(-q, p, 0));
-		v.add(new Vertex(-q, p, 0)); v.add(new Vertex(q, p, 0));
-		v.add(new Vertex(q, p, 0)); v.add(new Vertex(s, s, s));
-		
-		v.add(new Vertex(s, s, s)); v.add(new Vertex(p, 0, q));
-		v.add(new Vertex(p, 0, q)); v.add(new Vertex(p, 0, -q));
-		v.add(new Vertex(p, 0, -q)); v.add(new Vertex(s, s, -s));
-		v.add(new Vertex(s, s, -s)); v.add(new Vertex(q, p, 0));
-		
-		v.add(new Vertex(s, s, -s)); v.add(new Vertex(0, q, -p));
-		v.add(new Vertex(0, q, -p)); v.add(new Vertex(-s, s, -s));
-		v.add(new Vertex(-s, s, -s)); v.add(new Vertex(-q, p, 0));
-		
-		v.add(new Vertex(p, 0, q)); v.add(new Vertex(s, -s, s));
-		v.add(new Vertex(s, -s, s)); v.add(new Vertex(0, -q, p));
-		v.add(new Vertex(0, -q, p)); v.add(new Vertex(0, q, p));
-		
-		v.add(new Vertex(0, -q, p)); v.add(new Vertex(-s, -s, s));
-		v.add(new Vertex(-s, -s, s)); v.add(new Vertex(-p, 0, q));
-		v.add(new Vertex(-p, 0, q)); v.add(new Vertex(-s, s, s));
-		
-		v.add(new Vertex(-p, 0, q)); v.add(new Vertex(-p, 0, -q));
-		v.add(new Vertex(-p, 0, -q)); v.add(new Vertex(-s, s, -s));
-		
-		v.add(new Vertex(-s, -s, s)); v.add(new Vertex(-q, -p, 0));
-		v.add(new Vertex(-q, -p, 0)); v.add(new Vertex(-s, -s, -s));
-		v.add(new Vertex(-s, -s, -s)); v.add(new Vertex(-p, 0, -q));
-		
-		v.add(new Vertex(s, -s, s)); v.add(new Vertex(q, -p, 0));
-		v.add(new Vertex(q, -p, 0)); v.add(new Vertex(-q, -p, 0));
-		
-		v.add(new Vertex(0, q, -p)); v.add(new Vertex(0, -q, -p));
-		v.add(new Vertex(0, -q, -p)); v.add(new Vertex(s, -s, -s));
-		v.add(new Vertex(s, -s, -s)); v.add(new Vertex(p, 0, -q));
-		
-		v.add(new Vertex(q, -p, 0)); v.add(new Vertex(s, -s, -s));
-		
-		v.add(new Vertex(-s, -s, -s)); v.add(new Vertex(0, -q, -p));
 	}
 	
 	public void handleTurning()
@@ -279,10 +104,7 @@ public class World
 	}
 	
 	public void step(double dt)
-	{
-		for (int i=0; i<v.size(); i++)
-			v.get(i).translate(pos);
-		
+	{		
 		dodeca.reposition(pos);
 		
 		handleTurning();
@@ -308,12 +130,5 @@ public class World
 		mh.update(gl);
 		
 		dodeca.render(gl);
-//		
-//		for (int i=0; i<v.size(); i++)
-//		{
-//			v.get(i).use(vertexBuffer, normalBuffer);
-//		}
-//		vertexBuffer.rewind();
-//		normalBuffer.rewind();
 	}
 }
