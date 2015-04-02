26a1eb3e086545c313ad525308e2bc75a105078e
patowen95 <patowen95@gmail.com>
2015-04-02 18:13:13 -0400

    Work on sandbox

--- HyperbolicSpace/src/net/patowen/poincaresandbox/Display.java
+++ HyperbolicSpace/src/net/patowen/poincaresandbox/Display.java
@@ -4,16 +4,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Display extends JPanel
+public class Display extends JPanel implements MouseMotionListener
 {
 	private static final long serialVersionUID = 1L;
 	
 	private DrawHelper helper;
+	
+	private Vector2 mouse;
 
 	public static void main(String[] args)
 	{
@@ -32,11 +36,17 @@ public class Display extends JPanel
 	{
 		setPreferredSize(new Dimension(480, 480));
 		helper = new DrawHelper();
+		
+		mouse = new Vector2();
+		
+		addMouseMotionListener(this);
 	}
 	
 	public void paint(Graphics g1)
 	{
 		Graphics2D g = (Graphics2D)g1;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
 		
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, getWidth(), getHeight());
@@ -48,6 +58,18 @@ public class Display extends JPanel
 	
 	public void draw(Graphics2D g)
 	{
-		helper.drawEuclideanCircle(g, new Vector2(), 1);
+		helper.drawEuclideanCircle(g, new Vector2(), 1, false);
+		helper.drawCircle(g, mouse, 0.3, true);
+		helper.drawLineSegment(g, new Vector2(0.5, 0), mouse);
 	}
+
+	@Override
+	public void mouseDragged(MouseEvent e)
+	{
+		mouse = new Vector2(helper.getX(e.getX()), helper.getY(e.getY()));
+		repaint();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {}
 }
--- HyperbolicSpace/src/net/patowen/poincaresandbox/DrawHelper.java
+++ HyperbolicSpace/src/net/patowen/poincaresandbox/DrawHelper.java
@@ -2,6 +2,7 @@ package net.patowen.poincaresandbox;
 
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 
 public class DrawHelper
 {
@@ -12,27 +13,33 @@ public class DrawHelper
 	{
 		originX = (width-1)/2.0; originY = (height-1)/2.0;
 		double min = Math.min(width-1, height-1);
-		scale = min/2.0;
+		scale = min/2.0-2;
 	}
 	
-	public void drawEuclideanCircle(Graphics2D g, Vector2 center, double radius)
+	public void drawEuclideanCircle(Graphics2D g, Vector2 center, double radius, boolean fill)
 	{
-		g.draw(new Ellipse2D.Double(getI(center.x - radius), getJ(center.y - radius),
-				radius*scale*2, radius*scale*2));
+		if (fill)
+			g.fill(new Ellipse2D.Double(getI(center.x - radius), getJ(center.y - radius),
+					radius*scale*2, radius*scale*2));
+		else
+			g.draw(new Ellipse2D.Double(getI(center.x - radius), getJ(center.y - radius),
+					radius*scale*2, radius*scale*2));
 	}
 	
-	public void drawCircle(Graphics2D g, Vector2 center, double radius)
+	public void drawCircle(Graphics2D g, Vector2 center, double radius, boolean fill)
 	{
-		double denom = center.squared() + radius*radius;
+		double denom = center.squared()*radius*radius - 1;
 		double newRadius = radius * ((center.squared() - 1) / denom);
 		Vector2 newCenter = center.times((radius*radius - 1) / denom);
 		
-		drawEuclideanCircle(g, newCenter, newRadius);
+		drawEuclideanCircle(g, newCenter, newRadius, fill);
 	}
 	
 	public void drawLineSegment(Graphics2D g, Vector2 v1, Vector2 v2)
 	{
-		
+		if (Math.abs(v1.x * v2.y - v2.x * v1.y) < 0.01)
+			g.draw(new Line2D.Double(getI(v1.x), getJ(v1.y), getI(v2.x), getJ(v2.y)));
+		//Vector2 w2 = v2.hyperTranslate(v1.times(-1));
 	}
 	
 	public double getI(double x)
@@ -43,5 +50,15 @@ public class DrawHelper
 	public double getJ(double y)
 	{
 		return y*scale + originY;
+	}
+	
+	public double getX(double i)
+	{
+		return (i-originX) / scale;
+	}
+	
+	public double getY(double j)
+	{
+		return (j-originY) / scale;
 	}
 }
